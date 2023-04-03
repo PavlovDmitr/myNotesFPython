8ab9e7eac8d475162b4e8ca8876146e67f7523ab
PavlovDmitriy <feelerok@gmail.com>
2023-04-03 20:54:45 +0500

    add main, modelNote

---
+++ note_model.py
@@ -0,0 +1,12 @@
+class single_note:
+
+
+    def __init__(self, date_create, title, msg):
+        self.index = 1
+        self.date_create = date_create
+        self.date_modify = date_create
+        self.title = title
+        self.msg = msg
+
+        pass
+    
